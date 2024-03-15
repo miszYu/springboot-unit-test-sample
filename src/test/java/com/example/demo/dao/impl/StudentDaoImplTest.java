7a53e7bd3c9e862b9f91d6236dcd13e9e95d1b10
Cliff <cf0919383@gmail.com>
2024-03-15 10:41:00 +0800

    調整20240315 10:40

--- src/test/java/com/example/demo/dao/impl/StudentDaoImplTest.java
+++ src/test/java/com/example/demo/dao/impl/StudentDaoImplTest.java
@@ -4,6 +4,7 @@ import com.example.demo.dao.StudentDao;
 import com.example.demo.model.dto.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
